b895b6289ad4bb894a6b6fa80cd757617565566c
Luis Zavala <cionagi@hotmail.cl>
2020-06-21 17:46:11 -0400

    UPDATE index.js page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 // Dependency
 import React from 'react'
-import { Col, Row } from 'antd'
+import Head from 'next/head'
+import { Col, Row, Typography } from 'antd'
+import Carousel from '@brainhubeu/react-carousel'
 
 // Componets
 import BasicLayout from '../components/layout/BasicLayout'
@@ -8,37 +10,78 @@ import Container from '../components/layout/Container'
 import Hero from '../components/Hero'
 import MenuCategory from '../components/MenuCategory'
 import Card from '../components/product/Card'
+import MapCustom from '../components/Maps/MapCustom'
+
+const { Title } = Typography
 
 export default function Home() {
   return (
     <BasicLayout>
-      <Hero title="Lorem Ipsum" textBtn="Mas Info" />
+      <Head>
+        <title>Carniceria Txileno - Inicio</title>
+        <meta property="og:title" content="Carniceria Txileno" key="inicio" />
+      </Head>
+      <Hero title="LA MEJOR CALIDAD SIEMPRE" textBtn="Realizar pedido" />
       <MenuCategory />
       <Container>
-        <Row gutter={16}>
-          <Col xs={12} sm={12} md={8} lg={6} xl={6}>
-            <Card />
+        <Row>
+          <Col xs={24}>
+            <Title>Selección De Carnes De Primera</Title>
           </Col>
-          <Col xs={12} sm={12} md={8} lg={6} xl={6}>
-            <Card />
+        </Row>
+        <Row>
+          <Col xs={24}>
+            <Carousel slidesPerPage={3} arrows infinite>
+              <div>
+                <Card />
+              </div>
+              <div>
+                <Card />
+              </div>
+              <div>
+                <Card />
+              </div>
+              <div>
+                <Card />
+              </div>
+            </Carousel>
           </Col>
-          <Col xs={12} sm={12} md={8} lg={6} xl={6}>
-            <Card />
+        </Row>
+        <Row>
+          <Col xs={24}>
+            <Title>Los mas vendidos</Title>
           </Col>
-          <Col xs={12} sm={12} md={8} lg={6} xl={6}>
-            <Card />
+          <Col xs={24}>
+            <Carousel slidesPerPage={3} arrows infinite>
+              <div>
+                <Card />
+              </div>
+              <div>
+                <Card />
+              </div>
+              <div>
+                <Card />
+              </div>
+              <div>
+                <Card />
+              </div>
+            </Carousel>
           </Col>
-          <Col xs={12} sm={12} md={8} lg={6} xl={6}>
-            <Card />
+        </Row>
+
+        <Row>
+          <Col xs={24}>
+            <Title>Como comprar wizard</Title>
           </Col>
-          <Col xs={12} sm={12} md={8} lg={6} xl={6}>
-            <Card />
+        </Row>
+
+        <Row>
+          <Col xs={24}>
+            <Title>Zona de despacho</Title>
           </Col>
-          <Col xs={12} sm={12} md={8} lg={6} xl={6}>
-            <Card />
-          </Col>
-          <Col xs={12} sm={12} md={8} lg={6} xl={6}>
-            <Card />
+
+          <Col xs={24}>
+            <MapCustom />
           </Col>
         </Row>
       </Container>
